f96e9864598e73b714e5679dc4907dbe18c9bd3b
Zuzanna Sawala <zuzanna.sawala@student.put.poznan.pl>
2016-03-15 10:29:02 +0100

    category test

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "test should not create category without name" do
+    cat = Category.new
+    assert_not cat.save
+  end
+
 end
