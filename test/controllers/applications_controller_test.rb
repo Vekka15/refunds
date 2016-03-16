d46e33e3bc70f502b8c81d58647ee4d24f2090a7
Zuzanna Sawala <zuzanna.sawala@student.put.poznan.pl>
2016-03-16 19:16:55 +0100

    index controller app test

--- test/controllers/applications_controller_test.rb
+++ test/controllers/applications_controller_test.rb
@@ -1,9 +1,16 @@
 require 'test_helper'
 
 class ApplicationsControllerTest < ActionController::TestCase
-  test "should get index" do
+  test "should get index when there is session" do
+    sign_in User.first
     get :index
     assert_response :success
-    assert_nil assigns(:applications)
+    assert_not_nil assigns(:applications), "Index returned no applications"
+  end
+
+  test "should get index when there is no session" do
+    get :index
+    assert_nil assigns(:applications), "Index returned applications "
+    assert_redirected_to new_user_session_path, "There was now redirection to login page"
   end
 end
