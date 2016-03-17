20e60ba4ba98cbc218d434a20b97123646c33f59
Zuzanna Sawala <zuzanna.sawala@student.put.poznan.pl>
2016-03-18 00:32:42 +0100

    test sending

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,7 +1,7 @@
 require 'rufus/scheduler'
  scheduler = Rufus::Scheduler.new
 
- scheduler.at '2016/03/18 00:26:00' do
+ scheduler.every '1M' do
       puts "Test!"
       SendReportWorker.new.perform
       Rails.logger.info "hello, it's #{Time.now}"
