050ee00883d51158aa2b8ea160195577c6c4da44
Zuzanna Sawala <zuzanna.sawala@student.put.poznan.pl>
2016-03-18 00:09:52 +0100

    test sending emails heroku 5h

--- config/schedule.rb
+++
@@ -1,7 +0,0 @@
-# Use this file to easily define all of your cron jobs.
-
-# :day, :at => '12:20am'
-every 1.minutes do
-  puts 'dziala'
-  rake "send_report", :output => 'log/check_status_rake.log'
-end
