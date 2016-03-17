64fb33b2484beb9aaf908c45609d4fadbbf33df4
Zuzanna Sawala <zuzanna.sawala@student.put.poznan.pl>
2016-03-17 23:35:12 +0100

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,15 +80,30 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  ActionMailer::Base.delivery_method = :smtp
+#   ActionMailer::Base.delivery_method = :smtp
+#
+# ActionMailer::Base.smtp_settings = {
+#   :address              => "smtp.gmail.com",
+#   :port                 => "587",
+#   :domain               => "gmail.com",
+#   :user_name            => "vekka15@gmail.com",
+#   :password             => "G1607ka9ka",
+#   :authentication       => "plain",
+#   :enable_starttls_auto => true
+# }
 
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => "587",
-  :domain               => "gmail.com",
-  :user_name            => "vekka15@gmail.com",
-  :password             => "G1607ka9ka",
-  :authentication       => "plain",
-  :enable_starttls_auto => true
+config.action_mailer.default_url_options = { :host => 'dry-badlands-69193.herokuapp.com' }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = false
+config.action_mailer.default :charset => "utf-8"
+config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "gmail.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: "vekka15@gmail.com",
+  password: "G1607ka9ka"
 }
 end
