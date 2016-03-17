fd96c0c475e75845557186a2e00edff197973ab4
Zuzanna Sawala <zuzanna.sawala@student.put.poznan.pl>
2016-03-17 18:37:23 +0100

    fix travis error3

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,16 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  ActionMailer::Base.delivery_method = :smtp
+
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => "587",
+  :domain               => "gmail.com",
+  :user_name            => "vekka15@gmail.com",
+  :password             => "G1607ka9ka",
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
 end
