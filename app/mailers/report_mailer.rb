e8c632a2c1145e757e1a1e7f30bc3fae3ffeb207
Zuzanna Sawala <zuzanna.sawala@student.put.poznan.pl>
2016-03-18 15:07:44 +0100

    update user fixture

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -3,6 +3,7 @@ class ReportMailer < ApplicationMailer
     @current_month = Date.today.strftime("%m").to_i
     @current_year = Date.today.strftime("%Y").to_i
     @refundations = Refundation.where('extract(month from created_at) = ?', @current_month-1) #normalnie odjac jeden bo wysylamy 1 nastepnego miesiaca
-    mail(to: 'zuzanna.sawala@gmail.com', subject: "Report - #{Date::MONTHNAMES[@current_month]}, #{@current_year} ")
+    @admin_email = User.find_by_admin(true).email
+    mail(to: @admin_email, subject: "Report - #{Date::MONTHNAMES[@current_month]}, #{@current_year} ")
   end
 end
--- test/mailers/report_mailer_test.rb
+++ test/mailers/report_mailer_test.rb
@@ -2,7 +2,6 @@ require 'test_helper'
 
 class ReportMailerTest < ActionMailer::TestCase
   test "should add message to queue" do
-    # Send the email, then test that it got queued
     email = ReportMailer.report_email.deliver_now
     assert_not ActionMailer::Base.deliveries.empty?, "Did not added message to queue"
   end
